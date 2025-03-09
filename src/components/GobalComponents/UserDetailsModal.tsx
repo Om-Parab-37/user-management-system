60b3f1f3be8490fd2206cc91058be9255e578afc
Om-Parab-37 <omparab2682003@gmail.com>
2025-03-09 10:19:55 +0530

    fix(user-details):fix user deatails not showing for new users

--- src/components/GobalComponents/UserDetailsModal.tsx
+++ src/components/GobalComponents/UserDetailsModal.tsx
@@ -1,8 +1,9 @@
-import { Avatar, Card, Typography } from "antd";
+import { Avatar, Card, Empty, Typography } from "antd";
 import ModalComponent from "./ModalComponent";
 import { useQuery } from "@tanstack/react-query";
 import { getUserById } from "../../services/api/userApi";
 import { IUser } from "../../lib/types/userTypes";
+import { useUsers } from "../../stores/usersStore";
 const { Title, Text } = Typography;
 
 type UserDetailsModalProps = {
@@ -16,23 +17,29 @@ const UserDetailsModal = ({
   onClose,
   userId,
 }: UserDetailsModalProps) => {
+  const users = useUsers((state) => state.users);
+
   const { data: user } = useQuery({
     queryKey: ["user", userId],
     queryFn: () => getUserById(userId),
   });
+
+  const localUser = users.find((user) => user.id === userId);
+
   return (
     <>
       <ModalComponent isOpen={isOpen} onClose={onClose} title="User Details">
         <Card className="w-full max-w-md shadow-lg">
           <div className="flex flex-col items-center justify-between">
-            <Avatar size={100} src={user?.avatar} />
+            <Avatar size={100} src={user?.avatar || localUser?.avatar} />
 
             <Title level={3}>
-              {user?.first_name} {user?.last_name}
+              {user?.first_name || localUser?.first_name}{" "}
+              {user?.last_name || localUser?.last_name}
             </Title>
 
             <Text type="secondary" className="mb-5">
-              {user?.email}
+              {user?.email || localUser?.email}
             </Text>
           </div>
         </Card>
